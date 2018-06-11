bd9f9286dad8b3471939e30b97bbc4cb29315444
Voxel <39138467+tsvetinavasileva@users.noreply.github.com>
2018-06-11 21:43:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,25 +16,3 @@ bool mon(Set* sets[], int n){
     return true;
 }
 
-int main(){
-        while(true) {
-            IntRange *intRange = new IntRange(1, 5);
-            ArraySet *arraySet = new ArraySet(3);
-            arraySet->insert(6);
-            arraySet->insert(7);
-            arraySet->insert(9);
-            arraySet->insert(9);
-            int n = 2;
-            Set **sets = new Set *[n];
-            sets[0] = intRange;
-            sets[1] = arraySet;
-
-
-            std::cout << mon(sets, n);
-
-            delete[] sets;
-            delete intRange;
-            delete arraySet;
-        }
-
-}
